ce9dc00aa2e34a0c78dd43a1f91aa8d5b16d18ec
HazelW57 <hazel.hanyu.w@outlook.com>
2026-07-20 11:59:06 -0700

    Upload complete Saphiant website source and assets

---
+++ app/logo-data.ts
@@ -0,0 +1,247 @@
+export const assets = {
+  "saphiantLogo": "https://lh7-us.googleusercontent.com/sitesv-images-rt/ACHe0d01EbBQ0mw23iRduWvxaCSmpKqH12GSmuFX0iuyba2UCFp3D1ZRbHbU7JrHGel_I0IhvD1yug7vUjYW3uJpveVPjop5jdL33jUFNTc_SbZh8loqxWLLrXQP4IlyfQgR6EUfpSKJXjoCSrLtKt6WV2QRJPwtj4vxbVsUz6I0eHelshgFAfNDbdXp50JYuKc=w16383",
+  "brandLogos": [
+    {
+      "name": "Existing brand partner 01",
+      "category": "Consumer Electronics",
+      "src": "https://lh7-us.googleusercontent.com/sitesv-images-rt/ACHe0d3auv0I8E8GxtcRiWtoMNRZeSmv-TflAf36YUhaxGoSVl4syJVGYz7Tx_62yri-4P1BGThIMIrAQwkWPAwjSuVEBPt2oM9Vq7w6K74mMetpqEmRnkP9DIHHuEt6DXF0cl8PKd-A3gEK3IiqC7iOkJqx2TY6x_uRRdnfVqdCoQdSdMLvXDe4alVeXookEiA=w1280"
+    },
+    {
+      "name": "Existing brand partner 02",
+      "category": "Consumer Electronics",
+      "src": "https://lh7-us.googleusercontent.com/sitesv-images-rt/ACHe0d2Lge7UwN3m-VxEOsgJXkquin3k28yiIWetEHbmoXnNpBis3K--Rz9rlV93UGVqH8NMDUFr_qzLpqRSPYoNKG85YLmBhvULPZEufEP-VPrOQHauZsXvCKeRcG9qUuzunrWWP41lKRGr5Avafz3b7DVETOIOWH2-Z2rEuMSTd-KM9S-GHqzeyz9BaPX3Fac=w1280"
+    },
+    {
+      "name": "Existing brand partner 03",
+      "category": "Consumer Electronics",
+      "src": "https://lh7-us.googleusercontent.com/sitesv-images-rt/ACHe0d37pkcjIovflPvMUuGZPpE3HYbDRq8K4E-r4MU2QTM0vWUbyX7WNMXT5Re4MieP64B6hae7IeialvKBS3T6G-o5ULLR4GmXeIfWcOGhwFxEgR3yIVmPGdgYVssqwJDNiUFHRmWN5zdPenpCHcgxOPgme1xGUfKWrrnY9H4AtqJl6HrT_oQUFngYZgFgETI=w1280"
+    },
+    {
+      "name": "Existing brand partner 04",
+      "category": "Consumer Electronics",
+      "src": "https://lh7-us.googleusercontent.com/sitesv-images-rt/ACHe0d15-o9ed6D-CIm9k_kB3sE-u5TrzmrArcNO3J-yU7xOAapBNpunPbyHECfrb_7ZMMm1Q5S1s-JB4q5iW75uF77nUY4GDlOm39iDpIUD7X3Fyliv7CdxuKzjtjPq4QmArNqi1VSjpXQ86RciecyYPS3vidJHFfmsi5_-GZQ69WChjfSQ5sU6LKqLu6Y=w1280"
+    },
+    {
+      "name": "Existing brand partner 05",
+      "category": "Consumer Electronics",
+      "src": "https://lh7-us.googleusercontent.com/sitesv-images-rt/ACHe0d1uFP333UeKCfvvpEDUaEMGGV87FA7LZSjCEhXAMM7Xf7C1BHQSdcnm7iqbja3s5zwdJST5P9BepCmLlHIuBA8Cdm1lqdWXtGqkXUnYraUKOfOnvFZI8E2LJ7C-iMgCsu_FPGZRMR6V3aL_E9BHyn23jZyMwPNr18XH9EW7fdgmnnsJzs87Iq5vvE9HbgQ=w1280"
+    },
+    {
+      "name": "Existing brand partner 06",
+      "category": "Consumer Electronics",
+      "src": "https://lh7-us.googleusercontent.com/sitesv-images-rt/ACHe0d3ceE5BY2dA6bNMlNKcpvhZdPYoNywmJPk-bzxODDLUhLau4rJ-JcIA7aXYEHv1mHd-l20uykSJqIX6-S-W8PVHSrsXywQSIvMvs8_QU_RK9k5frZJdxJZExCjSJVs1-kr7LNQAP3Ja9KRR--F3febwpjoXjuvpvl0tW8SgwHo2K_t3pWUZf9BpDY-DWx4=w1280"
+    },
+    {
+      "name": "Existing brand partner 07",
+      "category": "Consumer Electronics",
+      "src": "https://lh7-us.googleusercontent.com/sitesv-images-rt/ACHe0d3g0fwYdbnh-ktQlHL6bVHOg2PmVJnNIwqW0bQt7KLhWqAq_B6JxyGox8COuXNM6OVv7jdXd8wTxACK1ZZUfoXERj6F_Rz27s6pblpatVQ_AuDDyFMOFSEgIFqntUeyhAXXNFUEkA7f0cva9N597AAPTgAdZLuEW_qyKhptIeY70Ds33zhnpI1KSlBN=w1280"
+    },
+    {
+      "name": "Existing brand partner 08",
+      "category": "Home & Lifestyle",
+      "src": "https://lh7-us.googleusercontent.com/sitesv-images-rt/ACHe0d2KgyUcWfs-VaPBFX99PAKICjR7V4zHbjnokGV0m1ax6setyeAdbL525WGb6zJVBwtzgvFOYXEg8L9Say5o2ktr7NgovPs-ba9T_kvw5lsRsnYBLs1jJ51mOUBDfG2aMxvySwVZIfa7Fxank7tk3ZlQKkPLBA7hV-He24avaZvQBd8iT5eF2aQPtSy5RAY=w1280"
+    },
+    {
+      "name": "Existing brand partner 09",
+      "category": "Home & Lifestyle",
+      "src": "https://lh7-us.googleusercontent.com/sitesv-images-rt/ACHe0d2hYiNo8afhRSAQYtfuTy6ROoZIWmZd2q0E1MGraXOuqIIkOLPLHj7yfxToCy3sdLgfFC8Cz0zTp2e-dLB7kcS1U37buV01ST5mnS1cOMpZpzaIsa4KLw5S2nN6vZ9PclnesZkT_EaaWMXK70t-NTBwTTb4pUgG8YwCov5jCReixm_9e6-j7tDZaSiJn2Q=w1280"
+    },
+    {
+      "name": "Existing brand partner 10",
+      "category": "Home & Lifestyle",
+      "src": "https://lh7-us.googleusercontent.com/sitesv-images-rt/ACHe0d1cuPMDunrAN-6KCvN8Ys4zEAxRxZounTUBPDFN8XFkQ1wysye4i5bzPLPV7unWnI12QJWT_i4ctailk6NZPJdQaz5vslO_ArBXoqGcl47LeIcoNrU3LNd2aYLZapEsM7bERqdY-JCo8v829zYpyuYn7Z_Kvju4CTGEMc2Yvmw8HU9gVMmz5Vuhdcg_=w1280"
+    },
+    {
+      "name": "Existing brand partner 11",
+      "category": "Home & Lifestyle",
+      "src": "https://lh7-us.googleusercontent.com/sitesv-images-rt/ACHe0d0gt0uZsprrx54l6CSW0znBOLOByxRInwwhUA1ZzQIyCwwoc72kMAnSrFJT30yY9clSaHXznJCIEjOIjwu2fCyr8aA54dIfXUQcRGl5bXxzz2WLp_YZiU3ZevmDAWb0MMn6Y_3oK4OLW2JAbN7T2arFTDXtOXEmsftNW2_HU4mB-vHLGUXuKLWA8eLN1IS8qsCYbDW0i9bWuzNxINYUnS72Fe76ZubGmDVy_thw=w1280"
+    },
+    {
+      "name": "Existing brand partner 12",
+      "category": "Home & Lifestyle",
+      "src": "https://lh7-us.googleusercontent.com/sitesv-images-rt/ACHe0d1fIhowd8fjOFPmVh-Bj92w3uPnpgH4TXuVfqANBa_I7uClBNFuM8reb9vne9LPBBi6iGnD7INUvY_2yU4LdiDTsO5K36NVNU0FTwejKGHO_Se3H7_VDCCYvlsZhYa9oKbUAVBhrZ-7BPnwQnREB0zH4bnn8-vo0bxmhr33efO47ZepPSWHFfrIS1W5bzU=w1280"
+    },
+    {
+      "name": "Existing brand partner 13",
+      "category": "Home & Lifestyle",
+      "src": "https://lh7-us.googleusercontent.com/sitesv-images-rt/ACHe0d2LDQxhCq5pD0bSq7AS9upzzIkKqfhVEVL8RpGa66LBNvbeBFsEFl9IQR6roQFi-KsLTjg8rGXnRRiF9S4Ll4ivO59Tng0ywWUqTwzGH84YGBjoU_f90xGr07gUyZJoaA_r1W4z1nle7RDkwK5cesC2lIbe5GbCPb_whwuBegNLeEsGPzVqT-KV2u3q=w1280"
+    },
+    {
+      "name": "Existing brand partner 14",
+      "category": "Home & Lifestyle",
+      "src": "https://lh7-us.googleusercontent.com/sitesv-images-rt/ACHe0d2BWF_PW3QBGG7mGMlf8YkwNA0RdINdxNRyCww5lgkNMr6bj2i4JgfKc64C5Rz9kVo1FdLeeZGWM_3u154e__MfxNL719mE5FI0UkdODklw-KAFSqIfT5MVRwe--BtI6rwcNdZ9LWqdDW7qfKiQTxNZSFNxEK1_bOWkFW3VLWvtokE8is2QZfH14u4zQlY=w1280"
+    },
+    {
+      "name": "Existing brand partner 15",
+      "category": "Mobility & Personal Care",
+      "src": "https://lh7-us.googleusercontent.com/sitesv-images-rt/ACHe0d1_mITkNgcpfSWo0yQkDhr8WDnr5rbnW2U68yHRYmWYKsucLdMp0ODwN01gpGSZ43ADZMopduNvn0v0MqglHAjk1005ky6Jy2yBdd8NQ6fy8UJ9GYMwdnWB2RolX5Je40Od3tqmG1aVqIXTQfm9RLk949OCO0L-V2QqPy6T7z1ozfBRk6Yn7922J7B9=w1280"
+    },
+    {
+      "name": "Existing brand partner 16",
+      "category": "Mobility & Personal Care",
+      "src": "https://lh7-us.googleusercontent.com/sitesv-images-rt/ACHe0d13orWZD5yR4uKZ0I3NX4ehDgnfgxYD59NAw_plV3wiO0rDwULJWKKh4xiNgoAvrJ68TtChR32gb_b9eUk3aTn1bLzKzKeGZ6VWqZNmG3SOG3UVDQhXMZL9lh2r52FPCZex76o3Z-GjdLNIodJQzMQAP4j5EB756JOS-4g2MAFiuGbEJ3XXRpiHvap60ig=w1280"
+    },
+    {
+      "name": "Existing brand partner 17",
+      "category": "Mobility & Personal Care",
+      "src": "https://lh7-us.googleusercontent.com/sitesv-images-rt/ACHe0d12i1gsV2AlCkJ0eS71SB2HvWAkRM_DuutawqPiqiwH0j2OSluCJFEEfITibJMjLYwGrBepRUUsgYT2cSQp8XJ0Pa3-kOIcRsxF-LeJInMhqZUAlHGQAZJu_yUEJflz453FoW4UGfX1qSqsOJAY3xj9pqu2StETg_alZvdzZ1XGGIOkfnmb3Rd-JyaTfQs=w1280"
+    },
+    {
+      "name": "Existing brand partner 18",
+      "category": "Mobility & Personal Care",
+      "src": "https://lh7-us.googleusercontent.com/sitesv-images-rt/ACHe0d13WzdjQuGQ1d8fkkp_bI3oS7zoRx_QgYMwmDf_sR3mVQQx44HkXFjGiLj6GJ8W1vCx10ggw-EnGqHp_11uTL7_giJFUPOjsuHqoTEkN66xCPks-TlDQ4R7v-xnPffBUnW74RGxkakSK2_nOyTWXlUmSVay_5b_FeUhbAhb6TaGCXW254k4IdvujoAcuepDmLuEFfkoaGEZc2d3TajjdZ-sCSmeBbicBk4hMFeR=w1280"
+    },
+    {
+      "name": "Existing brand partner 19",
+      "category": "Mobility & Personal Care",
+      "src": "https://lh7-us.googleusercontent.com/sitesv-images-rt/ACHe0d2Aj2OIGEs7MO00Lsoq7xLUbDjISbNtQjqZBHFe4eh77ZTRZIF7Sx4t2i5rr1MxROZivowMi0tUkZFlZm8xOR8gqkJ8QSYYvbCs97wrKwak9DTIkGRokQ-rJXzjZSP1sVD-MkmbyjHhMAQK7fV1oNyRzcbQENG317dVd_PRXgx1rVoOHlNrQe9wVnMP=w1280"
+    },
+    {
+      "name": "Existing brand partner 20",
+      "category": "Mobility & Personal Care",
+      "src": "https://lh7-us.googleusercontent.com/sitesv-images-rt/ACHe0d1JtDfOJkC_ZVuFCp3FPtWV533BTo-0TOaC6RtEPEq1eLj0BVgfmRDoG9My12RkJyozdm5UQPnTcZzeJrU5jTrxk1ryBKPbVJhA_YB5K80rwSSMhYk6kA3kKhB5HuIEX43GXGeFlAmD9R5sapJ-M2A36fbO0CA2H9gYWu5T1-Ur-GxOQAjKZExXkHKg6Xw=w1280"
+    },
+    {
+      "name": "Existing brand partner 21",
+      "category": "Mobility & Personal Care",
+      "src": "https://lh7-us.googleusercontent.com/sitesv-images-rt/ACHe0d1x9X9Q9h895JECJzBzUULbU7hrssCeGhniAdyrU1Gdzgjxtjaz8gFse4MSpiMlPT6M4ZB6rNvMZ05Ev6j3rWsnmLu8bLFebZegie12MxRuki288rJbIvkuYwrdFj2LP2KTNLP5-TxO_n9q7Azy6QlcJjhRLDmya4z8QzKgCg3qovsv_YN1xM3H4vQMCXfWCPlh3_nyVUeY-5HN0OBfHkmgagROsvVkJR6celyyxyc=w1280"
+    }
+  ],
+  "channelLogos": [
+    {
+      "name": "Existing sales channel 01",
+      "category": "Major Retail",
+      "src": "https://lh7-us.googleusercontent.com/sitesv-images-rt/ACHe0d05Aa7G0joSTXqd1duLThBx7ejng4atgUK1STelfHBtENpyhvm8vdWQW41YDb4-WDIY85K1nIlyggrb9Ol9_PS1oM9tWbhbMHRF9Wp2GOQd5WTge0qnuO_YoeiSy_PXjVVBmd2zDqDeas_ZacmI45hY0uxSVKc1V7ia7LdHaOMEsGOUznQETrThoDem3ys=w1280"
+    },
+    {
+      "name": "Existing sales channel 02",
+      "category": "Major Retail",
+      "src": "https://lh7-us.googleusercontent.com/sitesv-images-rt/ACHe0d1JC1FtZXyka3KVPaS6UARTxGilOsDB_KorBTtbCiRbfycb5yNKg-60R0tp0asSe0aTWGuWbMxk8VeJqEwsSpDTYnKGv6Ob4OXsOuz8n4Hu2uA3qZ8-BpB9uL7pbe6jv30jLVPuQK94L4-G8RJz0ldvYGkKKfsKS2eLnrgGvNdg4CXSnny-LH18OvWbX9U=w1280"
+    },
+    {
+      "name": "Existing sales channel 03",
+      "category": "Major Retail",
+      "src": "https://lh7-us.googleusercontent.com/sitesv-images-rt/ACHe0d0AoItOULSNP5ZwfRffWpDQQLgITzfGB5c2wyUYw0SrHxXRBp3Q_3cXIw5dqadzmwFP5kXUaB9AyYS6l2Hhr317nRDdH64wWJ-YET1zHug6qKdrwf4FcFrRtc36DCzYSD0VzGW0ToLhaFYm-RI_Jk1tTS5PKU4WoAoOfYo7oEDJNbi7HzNq-0X2d_VJJ4E=w1280"
+    },
+    {
+      "name": "Existing sales channel 04",
+      "category": "Major Retail",
+      "src": "https://lh7-us.googleusercontent.com/sitesv-images-rt/ACHe0d2iEHTehHJb4gFPW3lK099-1-F0G1jaeaQAAzoNz1qc1JAgkhgXdSFaW9oCe5RSNWkf18ZbR-4QUKHPv4Z2OM0kNG4CDe4DRgGPWyG8Zc8O0opAHBM2AfO4vkiiR6oo5FQG6Sf7zZPWB_7nv3vsJvYqiRFRjegyNM5nhu0oXnuwNUyaQf6QUerWQfeN=w1280"
+    },
+    {
+      "name": "Existing sales channel 05",
+      "category": "Major Retail",
+      "src": "https://lh7-us.googleusercontent.com/sitesv-images-rt/ACHe0d3IdM8YDJya-OyESj8-Rbfww7g1EiBTfo15pzetJk0WkpgKjXYWh8v9eiWaQlHSroMfkVgWUpQVkraS_eotZX4c8JwBKumMOT7sduMF_I9TAWDM1BlnUaKsOSKpxLAIS-W_PBd51YXeuFm3GKxrVydbtsCnCbU465xmSfshmpn0IKHsDPcEViURAwMyv8c=w1280"
+    },
+    {
+      "name": "Existing sales channel 06",
+      "category": "Marketplaces",
+      "src": "https://lh7-us.googleusercontent.com/sitesv-images-rt/ACHe0d1-AOowSz8jdz9cun-pugFx_jHc9iWBHl8MRhZ42IS14smQJY40-FUXB2vm1xkxQOCFYGAXV4J0pvFTZ0ZM899a5XiLBgjwyMDIzO19GNSro6BJ15gXxq6v_WgJrY3smKetDAD0E3NHRZJ048Eyw1KiLnpuj35jnkApnULA8ItL4wAyVHjhhDxuzU_n=w1280"
+    },
+    {
+      "name": "Existing sales channel 07",
+      "category": "Marketplaces",
+      "src": "https://lh7-us.googleusercontent.com/sitesv-images-rt/ACHe0d2mic5HyB9MT7uwo-xrwTyFOA0JoJrdCsr0a5tPSYwqID6Iup8BELU2jw56mq4jUXc-7L2mfdd1c_KHjvXD0VmIjuk6GzS20aTNTrAfgwHUGt8puyw4A0M5lKeULXBH1Vs8KZ6oozYXLBFWclJYAlgC3CVHNkK9bc1yXbZ0FigYytBic2cWc7f-GwYH0M8=w1280"
+    },
+    {
+      "name": "Existing sales channel 08",
+      "category": "Marketplaces",
+      "src": "https://lh7-us.googleusercontent.com/sitesv-images-rt/ACHe0d16Xi7s3aiC3TrxlSlFouJGrf0tPnF324paOQWkke8pDKaHuH66NU_b9SP8o43KxWo1wKg9IR-H3LfTd_coHQWtpJP1ln4UfKUNXS50VASCZSq6mFqv2XNNOs-xg_TgCpwpwhcmqU3PIQ99tIwc4A0Q8vglo96H-rxEqNMUIujowIcc3Jf6PrBurlFn=w1280"
+    },
+    {
+      "name": "Existing sales channel 09",
+      "category": "Marketplaces",
+      "src": "https://lh7-us.googleusercontent.com/sitesv-images-rt/ACHe0d0FyZoQfjzAp56fNE5nJUCU35iQ45pUyeGONSy4xdlmvB5snlfrELu7vby68_O3KEJi5HxaYSvjeY5CYzRElNI1TlaTot5_27dr8nwL_7NB3SHN_Ek1JyqXMs4s_y_MCsD209Lwoy3zIXKySGN5xFdcgFLGoLNZ-6x-1E3iMM-94K5s_xGHVMVbFm3E=w1280"
+    },
+    {
+      "name": "Existing sales channel 10",
+      "category": "Marketplaces",
+      "src": "https://lh7-us.googleusercontent.com/sitesv-images-rt/ACHe0d1rK4EQ9NnsUV4qJY9Tv6uQ7cbhIddRpgjdEqBF_vKTqD0tEE8yyODPTMLtZVgiSwiXYIO_2R9KC3HqEf8VDtScsrzrJ4xfKGGIAHGlzwm5XxO3F1NVcOU9fk8ihFONCZ6BrOUeQkGochMfHTm2CBPo_1C7Dp5E6wY9jYgIYIY34O2D4MXXYPyXCIEzY0Y=w1280"
+    },
+    {
+      "name": "Existing sales channel 11",
+      "category": "Specialty Retail",
+      "src": "https://lh7-us.googleusercontent.com/sitesv-images-rt/ACHe0d25tsCe0GSr9p_xE5g4UGmzlQZeYv4_F6Yh4oLj6bLdbGiDo7CkBmHHvzXeTAQG3APQyapufC6bemZWRYFHu5Fb9P3iAWkkN9ugwEHD9sUDbMRPpQti-Icz5qC1gwFvRTe7uR6wBrH6FcbVg4l1Wbv64xh7k6mZGE9FWxM9vp0DKnJKbaDvGhU1PZAB5bY=w1280"
+    },
+    {
+      "name": "Existing sales channel 12",
+      "category": "Specialty Retail",
+      "src": "https://lh7-us.googleusercontent.com/sitesv-images-rt/ACHe0d0ga921_9MQwQ8AAAO7Gs1tg0VeQT-Vr3hzd6eixuKC6p4Ow-bDhY4rEnNJHu7u8YHRocLNPXaug87F7N93f_uvqBZR09kIfcp3wAValpg8s6qoZWBA-KM5GykaaS1v_OvRTmA786JiHl2-O89Dq7feoSpeOyBFySrOHTAOy-30h8zgd9kdz64JnqoVtXw=w1280"
+    },
+    {
+      "name": "Existing sales channel 13",
+      "category": "Specialty Retail",
+      "src": "https://lh7-us.googleusercontent.com/sitesv-images-rt/ACHe0d17LD4_ZmvZRefQndDfHTXPbjE7znUk7w3Y25udQqxC4UkffY5ywn0B9TEL-eCYubyYd-hQ5N349Zb0eUMsaNyO7vwr0Gxi3FynmxGI_FtG1LVnpMkjl3zuAXOZhIbNHbEn6darnvlbjPRaX0ynSum8IO81p0BmYXQjX6-4b0T6vHnvsDp0ZproF5puG18=w1280"
+    },
+    {
+      "name": "Existing sales channel 14",
+      "category": "Specialty Retail",
+      "src": "https://lh7-us.googleusercontent.com/sitesv-images-rt/ACHe0d3xKz8EC9caEnqabRl1vTyxZbXgg6rJy0SdNN3boJx7jJgAOiPwzV_O5NycRErjEMmi3UvBNdHEBzQnApAzVi14h7yymZw_O4W0LsPZ6JV88NJpBc0EHLubIA9EPSWHEQwv6ANFH9qAXN0B5yAxaAdYeCAGmxl9Lflnt0toRUs4B1YqGQyQC4SqBmWP6Y0=w1280"
+    },
+    {
+      "name": "Existing sales channel 15",
+      "category": "Specialty Retail",
+      "src": "https://lh7-us.googleusercontent.com/sitesv-images-rt/ACHe0d0iUu-j98fMphrxVWdqcFI7y9mZd5iuGF76-wdwzXkxHRWU5TvkI1E1SK0vMIp0gqf5fMiFAGHD0EzT8D4CwPu5HSqF-0otFBf9qnT_iYPuYnUBqyyS-pr3kVxna7N8jn-KplUd54s0JVwvCB0JeCTm9OM2unOdbkNQKKo-vid9Ha2k3VmNP8Ykz9ub2jI=w1280"
+    },
+    {
+      "name": "Existing sales channel 16",
+      "category": "B2B & Wholesale",
+      "src": "https://lh7-us.googleusercontent.com/sitesv-images-rt/ACHe0d0HjpoF4GdF9-BUfzAF4doTsl_ZwHlajx_ebeDmG_Kfslsj7zRrCFKZiT4QePMQOehQ6QWuwmUm1YqLDI-zwmPcNYxv_m0zP3Cn8pNOzsnrVyFQaiLhmb_IDlB0OPFuK0G9s4l5aQB-53GW3TvKW7m7AQznQybOhxnOa1lngiY0OqPtArkV0RaYt_FdZQY=w1280"
+    },
+    {
+      "name": "Existing sales channel 17",
+      "category": "B2B & Wholesale",
+      "src": "https://lh7-us.googleusercontent.com/sitesv-images-rt/ACHe0d3aXxrViooHdMjtHRZBBSn_U5SPbU5sjouQTG_xz1Kirvr5urofSg8Rbhx3dneRWoJJPEK7WcvL-AS_4nUaxUW5VZJ_mpE3iLRpg83IRjecO9wqpkrt_BOejtCy8X23gIHa6ightYvKC3ZdXLFgJdY4kCLDsUi3DWl2G6zxGdd9IWPDt-Z8wljbllWZg3o=w1280"
+    },
+    {
+      "name": "Existing sales channel 18",
+      "category": "B2B & Wholesale",
+      "src": "https://lh7-us.googleusercontent.com/sitesv-images-rt/ACHe0d2jnsqYM5oy6UA0oE_CA36_fJpbX0uIO2RAnJZer1OT_JFOFrVGMcjIqZGCFZP0Va0mPhnYi-1mATaf-TkbIOyr7MIA9-Xf0VwonuHDMux_FNyVCVXBXnhcGkrQUHdi3SaPz3OlhmEIIF59G794CSOHqb-lcspNlRgfWgFJlJsewqAwtzWKXLtjKWv6=w1280"
+    },
+    {
+      "name": "Existing sales channel 19",
+      "category": "B2B & Wholesale",
+      "src": "https://lh7-us.googleusercontent.com/sitesv-images-rt/ACHe0d29HQc3Caeo4e21EVJdNOo1u4n43bXXHSh8t_OhvoaqNAA1HJAz92c51naZJAnJ803wAmVTWIZYYLSjQXC_OXM4Lq-sBchSNicgk8prjIKcpH-NeQJf0skJ5l_K-Na258Pet3d5aoCixmm73sVcLLdvibitNQ7NyKNXq0nvM2pF78ny5RMeVrP_o2QA914=w1280"
+    },
+    {
+      "name": "Existing sales channel 20",
+      "category": "Recommerce",
+      "src": "https://lh7-us.googleusercontent.com/sitesv-images-rt/ACHe0d0Z7Pp6fgpOYIuTVQJDP10iNUpIdod50zzfsBKKPjys7_Nu9eIA0viYyueXKScdPPmnMWBKfcPegSMh5cQz1jGFUNwyxMOXllzOA7-0P1rFyOWYqT8oXc8SHBkuf9O_PUs3r0a0AAe685vRyRxNBQEYryMTYx3OToa_RpD0a53MbSIgYzc18ZOhE3AE=w1280"
+    },
+    {
+      "name": "Existing sales channel 21",
+      "category": "Recommerce",
+      "src": "https://lh7-us.googleusercontent.com/sitesv-images-rt/ACHe0d2t-a6PGeqYjw-t6FQTpIVzXEV2aypCilMY3hSLx3_S8ev7dEqbNhiI5jdCOhhE66Qf6-hxjhLh0soCCBP_yhD2qFNON5OP7YHmJ-oR5cJRpyEZaHjT7I8ns1AbKSalVkwJ7NMJ08-MnxU5FAdxWDsccLGnlV0OcoLpvf3hVpq5G5zhJKEQTHvSRQrHpbg=w1280"
+    },
+    {
+      "name": "Existing sales channel 22",
+      "category": "Recommerce",
+      "src": "https://lh7-us.googleusercontent.com/sitesv-images-rt/ACHe0d10wm5XonxUp-jciF7w37JTWIgUL7KkjqDxz8s6jCWAjHuHQV-p_sB-luUDCUAXpj1AeAKguxQzpcKu8benq94SAUv9g4VI88hT-MJPEGgFK8vivDNN3EbpI1_DYToUbQ1l-Pmeobi1XL0UC9Ke-9Per8ro9lB35FUZZzwNG-2n4iXGmaEBjJ1ofIdt=w1280"
+    },
+    {
+      "name": "Existing sales channel 23",
+      "category": "Recommerce",
+      "src": "https://lh7-us.googleusercontent.com/sitesv-images-rt/ACHe0d03PDLFANJjDgOq-kjPyqZBIi3YtIQIjOxiHU1UUyQjbzt-x7Yb2ptaLy87WQcHqNH8vTSMPXW3NErTmvfB6t3BV7_rwpkVN6blPpzb2I23LkaczDm0WoQmeUh_0RdOhszAGmn5SIfOMjgeolIliG4Duzk9sVWZetnj0bHIZM1Q7zgtXiO2DK-in6845z0=w1280"
+    },
+    {
+      "name": "Existing sales channel 24",
+      "category": "Strategic Programs",
+      "src": "https://lh7-us.googleusercontent.com/sitesv-images-rt/ACHe0d3m7bxQm-m8j-ZQUWYoapcyxjQHENxOlJ5ZqN6O1sccJK_ozQ3XgqqdFkXxMW0inR_UI93h90z1yKixVNV5hRWppNxErtjjkNbxF3m0bYgDpGYKKAePXDZKn4_tT5X9MbIrLW63-egO-qwnDXprduZJTYKcco-inPgrlzA11RC4J5hX0Y43lrkkUqNlvGs=w1280"
+    },
+    {
+      "name": "Existing sales channel 25",
+      "category": "Strategic Programs",
+      "src": "https://lh7-us.googleusercontent.com/sitesv-images-rt/ACHe0d3FrWci2uMpmTkGRLukaLLozJTPDvv2MQxohjxgno3ib263YePyMsdLBfChoadIniPxFQLCZSKI6sZMWqPUOkkgVnqysUUmH10ipDEZ_q_u_t7ecV-o6npF9n1YYCnSfFYX1s8tXP9ZeA5OMGTawN5-AjCwRSahsDHBz8o9Z-Ztt8xNIBA7HGSI5rOVpCM=w1280"
+    },
+    {
+      "name": "Existing sales channel 26",
+      "category": "Strategic Programs",
+      "src": "https://lh7-us.googleusercontent.com/sitesv-images-rt/ACHe0d3kh8bieI47R9e4Qs4Ggm3YsHjeF2Ueyy5A5SfJ35vup9mC5XIgDTQiYWgFVGfKvWEBxI_d20vTLqZulxHx3CTc3ZWUcMcrGCOLaQbtqKK22PWsaKKCADxfj0DtSOxpKffylI49kVtpaQnDU9fh0fD7Pe8P12A1ZcFGu0E9jJ1dU-SulR4gYLNI4IZl=w1280"
+    },
+    {
+      "name": "Existing sales channel 27",
+      "category": "Strategic Programs",
+      "src": "https://lh7-us.googleusercontent.com/sitesv-images-rt/ACHe0d2lrz671VqbyelMW2pQ_Wrw2d6BC5kdZY8S98pUrL-LEms0pjRjFMnFyY4OMu1zYMdRTkfrcLIDetKfEMarXGqjyQZkLaSOB6i2P_BLqVhsOEfZWP88lscpe8wvZfeYbd5OUw_1wCTfn642dlGcqINdFrz5ARJUXi_pgLqZmUN1vuuXhL1kO_bt6TXRLgr8GMrqbtxa42GuojGjWzkwrrHdz8OXwCdmIaLkSGEqg7Q=w1280"
+    }
+  ]
+};
---
+++ app/page.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { useRef, useState } from "react";
+
+const logo = "/assets/saphiant-logo.png";
+
+const capabilities = [
+  ["01", "Retail & Marketplace Distribution", "Expand across retail programs, marketplaces and specialty channels with coordinated inventory, pricing and execution."],
+  ["02", "U.S. Fulfillment & Operations", "Support receiving, storage, fulfillment, drop, ship programs and channel, specific operational requirements."],
+  ["03", "Recommerce & Inventory Recovery", "Recover value from returned, excess and re, certified inventory through structured inspection, grading and resale programs."],
+  ["04", "Brand & Channel Management", "Coordinate listings, promotions, inventory allocation, channel communication and ongoing execution."],
+];
+
+const reasons = [
+  ["01", "Retail, Focused Experience", "Practical experience supporting major retail programs, marketplaces and channel partners."],
+  ["02", "Local U.S. Support", "A Los Angeles, based team enabling faster communication, coordination and issue resolution."],
+  ["03", "Integrated Operations", "Distribution, fulfillment, marketplace execution and recommerce managed through one operating structure."],
+  ["04", "Disciplined Channel Control", "Structured pricing, inventory allocation and channel, specific execution designed to protect brand value."],
+];
+
+const brandLogos = ["dreame","mova","hyperx","yaber","amazfit","meraw","yesoul","tp-link","cudy","segway","blutron","actbest","gitryin","buture","honiture","outin","toputure","arivea","conano","linner","mondo"].map(name => ({name, src:`/assets/brands/${name}.png`}));
+const channelLogos = ["tiktok","temu","target","amazon","mgm-resorts","walmart","ebay","lowes","micro-center","woot","best-buy","loves","bh","home-depot","sams-club","qvc","hsn","newegg","benlink","dicks","verdi"].map(name => ({name, src:`/assets/channels/${name}.${name === "temu" ? "jpg" : "png"}`}));
+
+function LogoRail({items}:{items:{name:string;src:string}[]}) {
+  const rail = useRef<HTMLDivElement>(null);
+  const move = (direction:number) => rail.current?.scrollBy({left:direction * Math.min(900, window.innerWidth * .72), behavior:"smooth"});
+  return <div className="railShell"><div className="railControls"><button onClick={()=>move(-1)} aria-label="Scroll logos left">←</button><button onClick={()=>move(1)} aria-label="Scroll logos right">→</button></div><div className="logoRail" ref={rail}><div className="logoRows">{items.map(item=><figure className="logoTile" key={item.name}><img src={item.src} alt={item.name.replaceAll("-"," ")}/></figure>)}</div></div></div>;
+}
+
+export default function Home() {
+  const [open, setOpen] = useState(false);
+  return <>
+    <header className="header"><a className="brand" href="#top"><img src={logo} alt="Saphiant Enterprises" /></a><button className="menu" onClick={()=>setOpen(!open)} aria-expanded={open} aria-label="Toggle menu">{open?"×":"☰"}</button><nav className={open?"open":""}><a href="#solutions">Solutions</a><a href="#brands">Brands</a><a href="#channels">Channels</a><a href="#operations">Operations</a><a href="#about">About</a><a href="#contact">Contact</a></nav><a className="headerCta" href="#contact">Partner With Us</a></header>
+    <main id="top">
+      <section className="hero"><div className="heroCopy"><p className="eyebrow">U.S. Distribution and Recommerce</p><h1>Scale Across U.S. Retail with Confidence</h1><p>Saphiant helps consumer brands expand through U.S. retail, marketplaces, fulfillment and recommerce channels with disciplined local execution.</p><div className="actions"><a className="button" href="#contact">Partner With Us</a><a className="button ghost" href="#solutions">Explore Our Capabilities</a></div></div><div className="network"><div className="grid"></div><article className="tag t1"><b>Retail & Marketplaces</b><span>Channel execution</span></article><article className="tag t2"><b>U.S. Operations</b><span>Fulfillment coordination</span></article><article className="tag t3"><b>Recommerce</b><span>Inventory recovery</span></article><i className="dot d1"></i><i className="dot d2"></i><i className="dot d3"></i></div></section>
+      <section className="trust"><span>U.S. Retail</span><span>Marketplace Operations</span><span>Fulfillment</span><span>Recommerce</span><span>Brand Support</span></section>
+      <section className="section light" id="solutions"><div className="heading"><div><p className="eyebrow">Integrated Capabilities</p><h2>Built for Multi, Channel Growth</h2></div><p>Practical support across the product lifecycle, from retail launch and marketplace execution to fulfillment and value recovery.</p></div><div className="cards">{capabilities.map(([n,h,p],i)=><article className="card" key={n}><span className="miniMark">{["↗","▦","↻","◎"][i]}</span><small>{n}</small><h3>{h}</h3><p>{p}</p></article>)}</div></section>
+      <section className="section dark" id="operations"><div className="heading"><div><p className="eyebrow">Why Saphiant</p><h2>Execution Beyond Distribution</h2></div><p>Local coordination, disciplined channel management and connected operations help products move with greater control.</p></div><div className="reasons">{reasons.map(([n,h,p],i)=><article key={n}><span className="miniMark">{["◇","⌁","＋","◌"][i]}</span><b>{n}</b><h3>{h}</h3><p>{p}</p></article>)}</div></section>
+      <section className="section processSection" id="recommerce"><div className="heading"><div><p className="eyebrow">Product Lifecycle</p><h2>Responsible Recommerce and Inventory Recovery</h2></div><p>Saphiant supports new, in, box, open, box, returned and re, certified inventory programs through structured operational processes and controlled channel deployment.</p></div><div className="process">{["Inventory Intake","Inspection","Functional Testing","Cleaning and Grading","Channel Allocation","Fulfillment and Reporting"].map((x,i)=><article key={x}><span>{String(i+1).padStart(2,"0")}</span><b>{x}</b></article>)}</div></section>
+      <section className="logosSection" id="brands"><div className="logoIntro"><div><p className="eyebrow">Brand Relationships</p><h2>Brands We Support</h2></div><p>Selected current and historical relationships across consumer electronics, home appliances, personal care and smart mobility.</p></div><LogoRail items={brandLogos}/><small>Selected current and historical brand relationships. Certain relationships may be product, program, or channel, specific.</small></section>
+      <section className="logosSection channelsSection" id="channels"><div className="logoIntro"><div><p className="eyebrow">Sales Channels</p><h2>Channel Experience</h2></div><p>Flexible access across retail, marketplace, specialty, wholesale and recommerce opportunities.</p></div><LogoRail items={channelLogos}/><small>Selected current and historical channel experience. Availability depends on product category, program requirements and partner approval.</small></section>
+      <section className="about" id="about"><p className="eyebrow">About Saphiant</p><h2>Built to Help Brands Scale with Confidence</h2><p>Founded in 2018 and headquartered in the Los Angeles area, Saphiant Enterprises supports brands and channel partners across distribution, retail operations, fulfillment and recommerce.</p><div className="stats"><span><b>2018</b>Founded</span><span><b>Los Angeles</b>Headquarters</span><span><b>U.S.</b>Fulfillment Network</span><span><b>New + Recommerce</b>Product Capabilities</span></div></section>
+      <section className="contact" id="contact"><div><p className="eyebrow">Start a Conversation</p><h2>Let’s Discuss Your U.S. Growth Strategy</h2><p>Tell us about your brand, products, target channels or inventory needs.</p></div><aside><small>Email our team</small><a href="mailto:Support@saphiant.com">Support@saphiant.com</a><p>13625 Marquardt Ave<br/>Santa Fe Springs, CA 90670</p></aside></section>
+    </main>
+    <footer><img src={logo} alt="Saphiant Enterprises"/><p>U.S. distribution, retail, fulfillment and recommerce solutions for consumer brands.</p><span>© 2026 Saphiant Enterprises LLC</span></footer>
+  </>;
+}
